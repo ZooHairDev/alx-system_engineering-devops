c5a0f7b3e06443981b6d1ef5c68025156191672a
ZooHairDev <zouhair.ob@gmail.com>
2023-11-27 11:31:00 -0800

    Update 0x0A

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
-# installing flask from pip3
-
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
 package {'flask':
   ensure   => '2.1.0',
-  name     => 'flask',
   provider => 'pip3'
 }
