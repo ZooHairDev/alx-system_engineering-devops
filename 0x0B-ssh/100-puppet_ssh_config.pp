64b2e2fae5c21cea2252cff01ef57322f652d44c
ZooHairDev <zouhair.ob@gmail.com>
2023-11-27 11:24:54 -0800

    Update 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,16 @@
-# Puppet configuration without password
+# Setting up my client config file
+include stdlib
 
-file_line{'turn off password authentication':
-path => '/etc/ssh/ssh_config',
-line => 'PasswordAuthentication no'
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
 }
 
-file_line{'declare identity file':
-path => '/etc/ssh/ssh_config',
-line => 'IdentifyFile ~/.ssh/school'
-
-
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
